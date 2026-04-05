d85c82bff9fa7198850ac5f2c176386b880babb8
Bolajare <bolajareinvestmentltd@gmail.com>
2026-04-05 13:16:10 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,6 @@ export default function Home() {
     <div className="bg-background min-h-screen">
       <Hero />
       <Experience />
-      <EventCategories />
       <Tickets />
       <Sponsors />
       <SocialProof />
