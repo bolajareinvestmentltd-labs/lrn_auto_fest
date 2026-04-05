58344dbe77ce3231c9cf27142f5431cec8478082
Bolajare <bolajareinvestmentltd@gmail.com>
2026-04-05 13:14:51 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@
 
 import Hero from '@/components/Hero';
 import Experience from '@/components/Experience';
-import EventCategories from '@/components/EventCategories';
 import Tickets from '@/components/Tickets';
 import Sponsors from '@/components/Sponsors';
 import SocialProof from '@/components/SocialProof';
