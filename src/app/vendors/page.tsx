b99562537bd7ee609d96e3ade25a432f9c2d78fd
Bolajare <bolajareinvestmentltd@gmail.com>
2026-04-05 19:16:15 -0400

    Update page.tsx

--- src/app/vendors/page.tsx
+++ src/app/vendors/page.tsx
@@ -398,7 +398,7 @@ export default function VendorPage() {
                                             Processing...
                                         </>
                                     ) : (
-                                        "Secure My Slot"
+                                        "PAY NOW"
                                     )}
                                 </Button>
                             </form>
